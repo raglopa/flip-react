bde445f17d4db87095fdc3cb2a87adce2b268777
Attila Polgar <raglopa@gmail.com>
2017-10-06 07:34:06 +0200

    feat(ga): add google-analytics

--- src/index.js
+++ src/index.js
@@ -3,8 +3,10 @@ import ReactDOM from 'react-dom'
 import './index.scss'
 import Root from './components/Root'
 import configureStore from './configureStore'
-
+import ReactGA from 'react-ga'
 import 'font-awesome/scss/font-awesome.scss'
+
+ReactGA.initialize('UA-107710846-1')
 
 const store = configureStore()
 
